afb3edfac9a1146f97ffa1c22919e4719ac50578
Katalyzator <web.coder96@gmail.com>
2016-05-21 18:10:55 +0600

    Add new Button

--- Day3/app/src/main/java/com/example/root/second_project/MainActivity.java
+++ Day3/app/src/main/java/com/example/root/second_project/MainActivity.java
@@ -9,25 +9,32 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 private Button mCrowsCounterButton;
-private int mCount = 0;
+private Button mCatCounterButton;
+private int mCountCrows = 0;
+private int mCountCat = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final TextView mInfoTextView = (TextView)findViewById(R.id.buttonCrowsCounter);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mCrowsCounterButton = (Button)findViewById(R.id.buttonCrowsCounter);
         mCrowsCounterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                mInfoTextView.setText("Я насчитал " + ++mCount + " ворон");
+                TextView mInfoTextView = (TextView)findViewById(R.id.textView);
+                mInfoTextView.setText("Я насчитал " + ++mCountCrows + " ворон");
+            }
+        });
+
+        mCatCounterButton = (Button)findViewById(R.id.buttonHello);
+        mCatCounterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView mInfoTextView = (TextView)findViewById(R.id.textView2);
+                mInfoTextView.setText(("Я насчитал " + ++mCountCat + " кошек"));
             }
         });
     }
 
-
-    public void onClick(View view) {
-        TextView helloTextView = (TextView)findViewById(R.id.textView);
-        helloTextView.setText("Hello Aibek!");
-    }
 }
