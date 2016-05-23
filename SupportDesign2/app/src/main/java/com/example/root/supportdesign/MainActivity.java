16c4d9dabcf3a41dab55296d8b80ef894a6946ed
Katalyzator <web.coder96@gmail.com>
2016-05-23 20:42:15 +0600

    Newlest method of changing background color

--- SupportDesign2/app/src/main/java/com/example/root/supportdesign/MainActivity.java
+++ SupportDesign2/app/src/main/java/com/example/root/supportdesign/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,7 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
-
+import android.support.v4.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Yes", snackbarOnListener);
 
                 View snackbarView = mSnackbar.getView();
-                snackbarView.setBackgroundColor(getResources().getColor(R.color.backgroundColor));
+                snackbarView.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.backgroundColor));
                 mSnackbar.show();
             }
         });
