68419ddc6ed725d348c5935f92ebb914e5774fce
Katalyzator <web.coder96@gmail.com>
2016-05-24 22:49:02 +0600

    Some files changed

--- Menu/app/src/main/java/com/example/root/menu/MainActivity.java
+++ Menu/app/src/main/java/com/example/root/menu/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.root.menu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        TextView infoTextView = (TextView) findViewById(R.id.textView);
+
+        switch (id) {
+
+            case R.id.action_about:
+                infoTextView.setText("You choose Action About");
+                return true;
+            case R.id.action_language:
+                infoTextView.setText("You choose Action Languages");
+                return true;
+            case R.id.action_settings:
+                infoTextView.setText("You choose Action Settings");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
